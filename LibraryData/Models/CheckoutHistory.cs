ccf23b0fed462b3e7906a4d8f6f447168d1ef808
Leonardo Viana <leonardo.viana@meep-app.com>
2019-12-13 17:35:58 -0300

    Add: Migration, Models, Context and Class Library

---
+++ LibraryData/Models/CheckoutHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryData.Models
+{
+    public class CheckoutHistory
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public LibraryAsset LibraryAsset { get; set; }
+        [Required]
+        public LibraryCard LibraryCard { get; set; }
+        public DateTime Checkout { get; set; }
+        public DateTime? CheckIn { get; set; }
+    }
+}
